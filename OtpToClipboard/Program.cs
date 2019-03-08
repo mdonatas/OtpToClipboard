704086208b69f007a1df62cb836016f3987cdd9f
Donatas Mačiūnas <mdonatas@gmail.com>
2019-03-08 17:31:11 +0200

    Set OTP to clipboard based on key passed in as a first parameter

---
+++ OtpToClipboard/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using OtpNet;
+
+namespace OtpToClipboard
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string otpKey = args.FirstOrDefault();
+#if DEBUG
+            if (string.IsNullOrEmpty(otpKey))
+            {
+                otpKey = "ASD";
+            }
+#endif
+
+            var bytes = Base32Encoding.ToBytes(otpKey);
+
+            var totp = new Totp(bytes, mode: OtpHashMode.Sha1);
+
+            string result = totp.ComputeTotp();
+
+            System.Windows.Forms.Clipboard.SetText(result);
+        }
+    }
+}
